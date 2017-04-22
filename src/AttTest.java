0440d1de99d4e24667240b7e7bd18d2ca7e729fe
Mihir2 <mihirchauhan9@gmail.com>
2017-04-21 22:53:19 -0400

    Update Coresets

--- src/AttTest.java
+++ src/AttTest.java
@@ -1,108 +1,49 @@
-import weka.core.Attribute;
-import weka.core.FastVector;
-import weka.core.Instance;
+//package greenblocks.statistics;
+ 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+ 
+import weka.clusterers.SimpleKMeans;
 import weka.core.Instances;
  
- /**
-  * Generates a little ARFF file with different attribute types.
-  *
-  * @author FracPete
-  */
- @SuppressWarnings("deprecation")
 public class AttTest {
-   public static void main(String[] args) throws Exception {
-     FastVector      atts;
-     FastVector      attsRel;
-     FastVector      attVals;
-     FastVector      attValsRel;
-     Instances       data;
-     Instances       dataRel;
-     double[]        vals;
-     double[]        valsRel;
-     int             i;
  
-     // 1. set up attributes
-     atts = new FastVector();
-     // - numeric
-     atts.addElement(new Attribute("att1"));
-     // - nominal
-     attVals = new FastVector();
-     for (i = 0; i < 5; i++)
-       attVals.addElement("val" + (i+1));
-     atts.addElement(new Attribute("att2", attVals));
-     // - string
-     atts.addElement(new Attribute("att3", (FastVector) null));
-     // - date
-     atts.addElement(new Attribute("att4", "yyyy-MM-dd"));
-     // - relational
-     attsRel = new FastVector();
-     // -- numeric
-     attsRel.addElement(new Attribute("att5.1"));
-     // -- nominal
-     attValsRel = new FastVector();
-     for (i = 0; i < 5; i++)
-       attValsRel.addElement("val5." + (i+1));
-     attsRel.addElement(new Attribute("att5.2", attValsRel));
-     dataRel = new Instances("att5", attsRel, 0);
-     atts.addElement(new Attribute("att5", dataRel, 0));
+	public static BufferedReader readDataFile(String filename) {
+		BufferedReader inputReader = null;
  
-     // 2. create Instances object
-     data = new Instances("MyRelation", atts, 0);
+		try {
+			inputReader = new BufferedReader(new FileReader(filename));
+		} catch (FileNotFoundException ex) {
+			System.err.println("File not found: " + filename);
+		}
  
-     // 3. fill with data
-     // first instance
-     vals = new double[data.numAttributes()];
-     // - numeric
-     vals[0] = Math.PI;
-     // - nominal
-     vals[1] = attVals.indexOf("val3");
-     // - string
-     vals[2] = data.attribute(2).addStringValue("This is a string!");
-     // - date
-     vals[3] = data.attribute(3).parseDate("2001-11-09");
-     // - relational
-     dataRel = new Instances(data.attribute(4).relation(), 0);
-     // -- first instance
-     valsRel = new double[2];
-     valsRel[0] = Math.PI + 1;
-     valsRel[1] = attValsRel.indexOf("val5.3");
-     dataRel.add(new Instance(1.0, valsRel));
-     // -- second instance
-     valsRel = new double[2];
-     valsRel[0] = Math.PI + 2;
-     valsRel[1] = attValsRel.indexOf("val5.2");
-     dataRel.add(new Instance(1.0, valsRel));
-     vals[4] = data.attribute(4).addRelation(dataRel);
-     // add
-     data.add(new Instance(1.0, vals));
+		return inputReader;
+	}
  
-     // second instance
-     vals = new double[data.numAttributes()];  // important: needs NEW array!
-     // - numeric
-     vals[0] = Math.E;
-     // - nominal
-     vals[1] = attVals.indexOf("val1");
-     // - string
-     vals[2] = data.attribute(2).addStringValue("And another one!");
-     // - date
-     vals[3] = data.attribute(3).parseDate("2000-12-01");
-     // - relational
-     dataRel = new Instances(data.attribute(4).relation(), 0);
-     // -- first instance
-     valsRel = new double[2];
-     valsRel[0] = Math.E + 1;
-     valsRel[1] = attValsRel.indexOf("val5.4");
-     dataRel.add(new Instance(1.0, valsRel));
-     // -- second instance
-     valsRel = new double[2];
-     valsRel[0] = Math.E + 2;
-     valsRel[1] = attValsRel.indexOf("val5.1");
-     dataRel.add(new Instance(1.0, valsRel));
-     vals[4] = data.attribute(4).addRelation(dataRel);
-     // add
-     data.add(new Instance(1.0, vals));
+	public static void main(String[] args) throws Exception {
+		SimpleKMeans kmeans = new SimpleKMeans();
  
-     // 4. output data
-     System.out.println(data);
-   }
- }
+		kmeans.setSeed(10);
+ 
+		//important parameter to set: preserver order, number of cluster.
+		kmeans.setPreserveInstancesOrder(true);
+		kmeans.setNumClusters(3);
+ 
+		BufferedReader datafile = readDataFile("TrainingData\\diabetes.arff"); 
+		Instances data = new Instances(datafile);
+ 
+ 
+		kmeans.buildClusterer(data);
+ 
+		// This array returns the cluster number (starting with 0) for each instance
+		// The array has as many elements as the number of instances
+		int[] assignments = kmeans.getAssignments();
+ 
+		int i=0;
+		for(int clusterNum : assignments) {
+		    System.out.printf("Instance %d -> Cluster %d \n", i, clusterNum);
+		    i++;
+		}
+	}
+}
--- src/hierClu.java
+++ src/hierClu.java
@@ -1,40 +1,73 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import weka.core.converters.CSVLoader;
 
 import weka.clusterers.HierarchicalClusterer;
+import weka.clusterers.SimpleKMeans;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
+
 public class hierClu {
+	
+	public static BufferedReader readDataFile(String filename) {
+		BufferedReader inputReader = null;
+ 
+		try {
+			inputReader = new BufferedReader(new FileReader(filename));
+		} catch (FileNotFoundException ex) {
+			System.err.println("File not found: " + filename);
+		}
+ 
+		return inputReader;
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
-		BufferedReader breader = null;
-		breader = new BufferedReader(new FileReader("TrainingData\\diabetes.csv"));
-		System.out.println(breader);
-		Instances train = new Instances(breader);
-		breader.close();
+		SimpleKMeans kmeans = new SimpleKMeans();
+		 
+		kmeans.setSeed(10);
+ 
+		//important parameter to set: preserver order, number of cluster.
+		kmeans.setPreserveInstancesOrder(true);
+		kmeans.setNumClusters(2);
+ 
+		BufferedReader datafile = readDataFile("TrainingData\\diabetes.arff"); 
+		Instances data = new Instances(datafile);
+ 
+		kmeans.buildClusterer(data);
+ 
+		// This array returns the cluster number (starting with 0) for each instance
+		// The array has as many elements as the number of instances
+		int[] assignments = kmeans.getAssignments();
+		int i=0;
+		for(int clusterNum : assignments) {
+			System.out.println(clusterNum + "======" + data.get(i));
+		    i++;
+		}
+		Instances instances = kmeans.getClusterCentroids();
+		System.out.println(instances);
+		
 		//Manhattan - true
 		//Euclidean - false
 		//--------------------------------------------------------------
 		HierarchicalClusterer hc = new HierarchicalClusterer(true);
-		hc.buildClusterer(train);
+		hc.buildClusterer(data);
 		System.out.println("Manhattan: " + hc.graph());
 		//--------------------------------------------------------------
 		HierarchicalClusterer hc1 = new HierarchicalClusterer(false);
-		hc1.buildClusterer(train);
+		hc1.buildClusterer(data);
 		System.out.println("Euclidean: " + hc1.graph());
 		//--------------------------------------------------------------
-		/*double[] l = new double[train.numAttributes()];
-		for(int i=0;i<train.numAttributes();i++){
-			l[i] = 0;
-		}
-		Instance inst = new DenseInstance(1, l);
-		hc.getDistanceFromZero(inst);*/
-		
 		
 	}
 }
